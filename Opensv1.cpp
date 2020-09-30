68d39039b29d91c2e728fc309ceb150f8967928a
Masya322 <37528741+Masya322@users.noreply.github.com>
2020-09-30 12:45:58 +0300

    Add files via upload

---
+++ Opensv1.cpp
@@ -0,0 +1,56 @@
+﻿#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+
+int main() {		
+		VideoCapture cap(3);		
+		
+		Mat frame;
+		Mat img;
+
+		while (1) {				
+							
+				// Capture frame-by-frame				
+				cap >> frame;			                                 //orig
+				// If the frame is empty, break immediately				
+				if (frame.empty())					
+					break;
+				// Display the resulting frame				
+				imshow("Frame", frame);
+			
+				cap >> img;                                             //conturi
+				Mat edges;
+				cvtColor(img, edges, CV_BGR2GRAY);
+				Canny(edges, edges, 30, 60);
+				imshow("window label", edges);
+
+				cap >> img;                                             //blur
+				Mat blr;
+				cvtColor(img, edges, CV_LOAD_IMAGE_COLOR);
+				blur(img, blr, Size(10, 10), Point(-1, -1), BORDER_DEFAULT);
+				imshow("Blur window",blr);
+
+				cap >> img;                                             //green filtr
+				Mat green;
+				cvtColor(img, green, COLOR_BGR2HSV);
+				inRange(green, Scalar(0, 50, 0), Scalar(255, 100, 255), green);
+				imshow("Green window", green);
+				
+				
+				// Press  ESC on keyboard to exit				
+				char c = (char)waitKey(25);			
+				if (c == 27)					
+					break;			
+		}
+		
+		
+		cap.release();					
+		destroyAllWindows();		
+		return 0;
+	
+}
+
